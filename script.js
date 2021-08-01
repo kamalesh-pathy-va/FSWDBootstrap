3490ccdfadefce4451aa0de550481d285fa27bf7
kamalesh-pathy-va <asokankamalesh@gmail.com>
2021-08-01 20:29:40 +0530

    Add the delete function

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const taskContainer = document.querySelector(".task__container");
-const globalStore = [];
-console.log(taskContainer);
+let globalStore = [];
 
 const generateNewCard = (taskData) => {
 	const newCard = `
-	<div class="col-sm-12 col-md-6 col-lg-4" id=${taskData.id}>
+	<div class="col-sm-12 col-md-6 col-lg-4">
 		<div class="card border-primary h-100">
 			<div class="card-header d-flex justify-content-end gap-2 border-primary">
 				<button type="button" class="btn btn-outline-primary"><i class="fas fa-pencil-alt"></i></button>
-				<button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+				<button type="button" class="btn btn-outline-danger" id=${taskData.id} onClick="deleteCard.apply(this,arguments)"><i class="fas fa-trash" id=${taskData.id} onClick="deleteCard.apply(this,arguments)"></i></button>
 			</div>
 			<div class="card-body">
 				<img src=${taskData.imageUrl} class="card-img-top rounded mb-2" alt="random 300x200 image">
@@ -25,12 +24,27 @@ const generateNewCard = (taskData) => {
 const loadInitialCardData = () => {
 	const getCardData = localStorage.getItem("taskyKam2610");
 	const {cards} = JSON.parse(getCardData);
-	console.log(cards);
 	cards.map((cardObject) => {
 		// console.log(cardObject);
 		taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
 		globalStore.push(cardObject);
 	});
+}
+
+const deleteCard = (event) => {
+	event = window.event;
+	console.log(event);
+	const targetID =  event.target.id;
+	const tagname = event.target.tagName;
+
+	globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
+	localStorage.setItem("taskyKam2610", JSON.stringify({cards:globalStore}));
+
+	if(tagname === "BUTTON") {
+		return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+	} else {
+		return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+	}
 }
 
 const saveChanges = () => {
